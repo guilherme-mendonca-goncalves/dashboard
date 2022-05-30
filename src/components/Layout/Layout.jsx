8933c706caf4dcdb39122256537f57b82572a885
Guilherme Mendonça Gonçalves <guilhermemg_93@hotmail.com>
2022-05-30 15:32:46 -0300

    Update sidebar

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,24 +1,21 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import Router from '../Router/Router'
 import Sidebar from '../Sidebar'
+import './Layout.css'
 
 
 const Layout = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route render={(props) => (
           <div className='layout'>
-            <Sidebar {...props} />
+            <Sidebar/>
             <div className='layout__content'>
               <div className='layout__content-main'>
                 <Router />
               </div>
             </div>
           </div>
-        )}/>
-      </Routes>
     </BrowserRouter>
   )
 }
--- src/components/Router/Router.jsx
+++ src/components/Router/Router.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { CustomersPage, DashboardPage } from '../../pages';
 
 const Router = () => {
   return (
-    <BrowserRouter>
       <Routes>
         <Route path='/' element={<DashboardPage/>} />
         <Route path='/customers' element={<CustomersPage/>} />
       </Routes>
-    </BrowserRouter>
   )
 }
 
