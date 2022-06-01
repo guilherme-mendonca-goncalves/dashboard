155596cba4416dfcf74dafceb6da41f09f51a7d2
Guilherme Mendonça Gonçalves <guilhermemg_93@hotmail.com>
2022-06-01 19:46:53 -0300

    Make table, badge and customers page

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as Layout } from './Layout';
 export { default as Sidebar } from './Sidebar';
 export { default as TopNav } from './TopNav';
 export { default as Dropdown } from './Dropdown';
+export { default as Table } from './Table';
